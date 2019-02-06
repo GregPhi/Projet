1cf7dbf4f8e463a4848403de7eac4a6c81505897
Philippot Gregoire <philippot@120p1.fil.univ-lille1.fr>
2019-02-06 13:33:58 +0100

    JavaFX : test de création d'un javafx.Stage + ajout readme

---
+++ JAVA/Password/Readme.md
@@ -0,0 +1,20 @@
+Projet Gestionnaire de mot de passe
+-----------------------------------
+
+Projet réalisé par :
+  * Philippot Grégoire
+
+### Outils utilisés
+
+  * Eclipse
+  * JavaFx [tutoriel](http://tutorials.jenkov.com/javafx/index.html)
+
+### But du projet
+
+
+### Utilisation
+
+*Compilation*
+
+*Exécution*
+
--- JAVA/Password/src/gestionnaire/Gestionnaire.java
+++ JAVA/Password/src/gestionnaire/Gestionnaire.java
@@ -1,5 +1,26 @@
 package gestionnaire;
 
-public class Gestionnaire {
-	
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Gestionnaire extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		Text text = new Text(10,40,"Hello world");
+		text.setFont(new Font(40));
+		Scene scene = new Scene(new Group(text));
+		primaryStage.setTitle("Hello");
+		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
 }
